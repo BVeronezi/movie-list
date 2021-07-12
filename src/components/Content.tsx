1506c36796f3e97447d24afb8f6ba0944457a823
BVeronezi <bds.veronezi@gmail.com>
2021-07-12 12:11:57 -0300

    Componentizando a aplicação

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+import { Header } from "./Header";
+interface ContentProps {
+  title: string;
+  movies: Array<MovieProps>
+}
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content({ title, movies }: ContentProps) {
+  return (
+    <div className="container">
+      <Header title={title} />
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
